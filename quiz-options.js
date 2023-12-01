c8169432f13890932a55de0cf3457a6d7d46cce4
Charlotte Lim <s10258748@connect.np.edu.sg>
2023-12-01 18:12:10 +0800

    index.html

---
+++ quiz-options.js
@@ -0,0 +1,94 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "When was Marilyn Monroe born?",
+    answer: "June 1, 1926",
+    options: [
+      "June 1, 1926",
+      "June 15, 1922",
+      "July 10, 1926",
+      "June 2, 1925"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What was Marilyn Monroe's birth name?",
+    answer: "Norma Jean Mortenson",
+    options: [
+      "Norma Jean Baker",
+      "Marilyn Monroe",
+      "Norma Jean Mortenson",
+      "Norma Jean Dougherty "
+    ]
+  },
+    {
+    numb: 3,
+    question: "3) Who was Marilyn Monroe's first husband?",
+    answer: "Jim Dougherty",
+    options: [
+      "Arthur Miller",
+      "Joe DiMaggio",
+      "Jim Dougherty",
+      "Ben Lyon"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How many volumes of books did Marilyn Monroe had on her bookshelves at the time of her passing?",
+    answer: "400",
+    options: [
+      "399",
+      "400",
+      "450",
+      "500"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How much was Marilyn Monroe's white dress from the film The Seven Year Itch sold for?",
+    answer: "$4.6m",
+    options: [
+      "$2.6m",
+      "$3.3m",
+      "$3.6m",
+      "$4.6m"
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "How many appearances in films did Marilyn Monroe made throughout her career?",
+    answer: "30",
+    options: [
+        "31",
+        "26",
+        "50",
+        "30"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which plastic surgery did Marilyn Monroe get?",
+    answer: "Chin and Nose",
+    options: [
+      "Lips and Jaw",
+      "Eyelid and Chin",
+      "Eyelid and Nose",
+      "Chin and Nose"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "When was the passing of Marilyn Monroe?",
+    answer: "4 August, 1962",
+    options: [
+      "5 August 1962",
+      "4 August, 1962",
+      "4 August 1963",
+      "6 August 1963"
+    ]
+  },
+];
